fc390da9b283b8e4f707fd588111b3afa5cb2d0b
lunafl1are <lunafl1are@protonmail.com>
2024-05-04 18:52:25 +0300

    fixed css issues

---
+++ cal_56/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    private int a;
+    private int b;
+    private String req_calculation;
+
+    public Main(int a, int b, String req_calculation) {
+        this.a = a;
+        this.b = b;
+        this.req_calculation = req_calculation;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public String getReq_calculation() {
+        return req_calculation;
+    }
+}
